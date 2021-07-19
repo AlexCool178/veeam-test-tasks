9c823f72b90f41df3c063bb83b22c74d18646879
Kulikov_AA <alcooverdrive@gmail.com>
2021-07-19 14:06:27 +0300

    removed "print" with logging

--- task1.py
+++ task1.py
@@ -1,7 +1,16 @@
 import os
 import sys
 import shutil
+import logging
 import xml.etree.ElementTree as ETree
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(filename='copy_module.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 
 def parse_config_file(config_filename):
@@ -9,18 +18,18 @@ def parse_config_file(config_filename):
         xml_tree = ETree.parse(config_filename)
         return xml_tree
     except FileNotFoundError:
-        print('Config file not found, check filename or path. Script terminated.')
-        sys.exit()
+        log.info('Config file not found, check filename or path. Script terminated.')
+        sys.exit('Config file not found, check filename or path. Script terminated.')
 
 
 def get_file_tags(xml_tree):
     root = xml_tree.getroot()
     file_tags = [child for child in root if child.tag == 'file']
-    if len(file_tags) > 0:
+    if file_tags:
         return file_tags
     else:
-        print('Tags <file> not found in config file. Script terminated.')
-        sys.exit()
+        log.info('Tags <file> not found in config file. Script terminated.')
+        sys.exit('Tags <file> not found in config file. Script terminated.')
 
 
 def get_valid_file_tags(file_tags):
@@ -33,25 +42,26 @@ def copy_files(valid_tags):
         for params_to_execute in valid_tags:
             source = params_to_execute[0] + '/' + params_to_execute[2]
             destination = params_to_execute[1] + '/' + params_to_execute[2]
-            print(f'Trying to copy {params_to_execute[2]} from {params_to_execute[0]} to {params_to_execute[1]}')
+            log.info('Trying to copy %s from %s to %s',
+                     params_to_execute[2], params_to_execute[0], params_to_execute[1])
             if os.path.isdir(params_to_execute[0]):
                 if os.path.isdir(params_to_execute[1]):
                     if os.path.isfile(source):
                         try:
                             shutil.copyfile(source, destination)
-                            print(f'Successfully copied file {params_to_execute[2]}')
+                            log.info(f'Successfully copied file {params_to_execute[2]}')
                         except PermissionError as e:
-                            print(str(e))
+                            log.info(str(e))
                     else:
-                        print('Source file not found')
+                        log.info('Source file not found')
                         pass
                 else:
-                    print('Destination folder not found')
+                    log.info('Destination folder not found')
                     pass
             else:
-                print('Source folder not found')
+                log.info('Source folder not found')
     else:
-        print('No valid tags in config file to operate. Script terminated.')
+        log.info('No valid tags in config file to operate. Script terminated.')
 
 
 if __name__ == '__main__':
